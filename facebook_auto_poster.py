888eb5c2a6fea7c84f2a55398b9ab8ae5ac557c6
Christopher Martinez <crmartinez239@protonmail.com>
2025-12-28 16:16:38 -0500

    Swap comma pos

--- facebook_auto_poster.py
+++ facebook_auto_poster.py
@@ -149,8 +149,8 @@ def post_to_page(page_id, access_token, message: str, booking_number: str, image
 def save_new_batch(new_batch: list[str]):
     batch_str = ",".join(new_batch)
     with open("last_batch.csv", "a") as file:
-        file.write(",")
         file.write(batch_str)
+        file.write(",")
 
 
 def main():
