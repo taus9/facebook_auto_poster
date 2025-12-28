41c030704509688e503cd37aa145bcedbdd05ae2
Christopher Martinez <crmartinez239@protonmail.com>
2025-12-28 12:27:09 -0500

    WIP: Start refactor

--- facebook_auto_poster.py
+++ facebook_auto_poster.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 """
-Facebook Auto Poster - Posts to a Facebook page every 24 hours
-Version 2 - Using environment variables for security
+Facebook Auto Poster
 """
 
-import time
+#import time
 import logging
 import os
 
-import schedule
+#import schedule
 from dotenv import load_dotenv
 
-from facebook_auto_poster_service import FacebookAutoPoster
+#from facebook_auto_poster_service import FacebookAutoPoster
 
 
 # Load environment variables
 load_dotenv()
-
 
 # Configure logging
 logging.basicConfig(
@@ -40,21 +38,21 @@ def main():
         logging.error("Missing required Facebook credentials. Check your .env file.")
         return
 
-    poster = FacebookAutoPoster(
-        access_token=page_access_token,
-        page_id=page_id,
-        arrests_api_url=arrests_api_url
-    )
+ #   poster = FacebookAutoPoster(
+ #       access_token=page_access_token,
+ #       page_id=page_id,
+ #       arrests_api_url=arrests_api_url
+ #   )
     
-    schedule.every().day.at(post_time).do(poster.scheduled_post)
+ #   schedule.every().day.at(post_time).do(poster.scheduled_post)
 
     logging.info("Facebook Auto Poster started. Posts scheduled daily at %s", post_time)
     logging.info("Press Ctrl+C to stop")
 
     try:
-        while True:
-            schedule.run_pending()
-            time.sleep(60)
+#        while True:
+#            schedule.run_pending()
+#            time.sleep(60)
     except KeyboardInterrupt:
         logging.info("Facebook Auto Poster stopped by user")
 
